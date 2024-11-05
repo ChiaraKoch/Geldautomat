d41bd41cac26e1fada364ad38c4d1816729b37bc
ChiaraKoch <cadmk@gmail.com>
2024-11-05 20:44:50 +0200

    Started the project

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+while True:
+    print("Welcome to Maryvale Life Teen Transfer!")
+    print("1. Deposit Registration Money")
+    print("2. Deposit Donation Money")
+    print("3. Withdraw Money")
+    print("4. Pay Bill")
+    
+    user = int(input("Choose Option: "))
+    if user == 1:
